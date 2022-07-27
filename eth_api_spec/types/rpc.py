ad34bf387c22406b70934257c78592562d63c635
banteg <4562643+banteg@users.noreply.github.com>
2022-07-27 13:18:34 +0400

    feat: add jsonrpc types

---
+++ eth_api_spec/types/rpc.py
@@ -0,0 +1,25 @@
+# https://www.jsonrpc.org/specification
+
+from typing import Any, Literal, Optional
+
+from msgspec import Raw, Struct
+
+
+class Error(Struct):
+    code: int
+    message: str
+    data: Optional[Any] = None
+
+
+class Response(Struct):
+    result: Optional[Raw] = None
+    error: Optional[Error] = None
+    id: Optional[str | int] = None
+    jsonrpc: Literal["2.0"] = "2.0"
+
+
+class Request(Struct):
+    method: str
+    params: Optional[Any] = None
+    id: Optional[str | int] = None
+    jsonrpc: Literal["2.0"] = "2.0"
