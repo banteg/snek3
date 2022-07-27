b4a1f2f4cd68ea708e90ec05c6aaddaadc3e8006
banteg <4562643+banteg@users.noreply.github.com>
2022-07-27 09:45:15 +0400

    fix: use literal false

--- eth_api_spec/client.py
+++ eth_api_spec/client.py
@@ -1,14 +1,14 @@
-from typing import Optional
+from typing import Literal
 
 from msgspec import Struct
 
 from eth_api_spec.types import uint
 
 
-class SyncingProgress(Struct, rename='camel'):
+class SyncingProgress(Struct, rename="camel"):
     starting_block: uint
     current_block: uint
     highest_block: uint
 
 
-SyncingStatus = Optional[SyncingProgress]
+SyncingStatus = SyncingProgress | Literal[False]
