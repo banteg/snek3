9458cf98b44be4461245c6d49a58b10dcc1a3b3f
banteg <4562643+banteg@users.noreply.github.com>
2022-07-27 12:25:55 +0400

    refactor: merge tx info, can't do complex unions

--- eth_api_spec/types/transaction.py
+++ eth_api_spec/types/transaction.py
@@ -1,18 +1,10 @@
 from typing import List, Optional
+from hexbytes import HexBytes
 
 from msgspec import Struct
 
 from eth_api_spec.types.base import address, byte, hash32, uint
 
-
-class AccessListEntry(Struct, rename="camel"):
-    address: address
-    storage_keys: List[hash32]
-
-
-AccessList = List[AccessListEntry]
-
-# field renames
 
 transaction_rename = {
     "type": "type",
@@ -38,85 +30,33 @@ transaction_rename = {
     "transaction_index": "transactionIndex",
 }
 
-# common properties
+
+class AccessListEntry(Struct, rename="camel"):
+    address: address
+    storage_keys: List[hash32]
 
 
-class TransactionBase(Struct, rename=transaction_rename.get):
-    type: byte
+AccessList = List[AccessListEntry]
+
+
+class TransactionInfo(Struct, rename=transaction_rename.get):
+    type: uint
     nonce: uint
+    sender: address
     to: Optional[address]
     gas: uint
     value: uint
-    input: bytes
-    chain_id: Optional[uint]
-
-
-# unsigned transactions
-
-
-class Transaction1559Unsigned(TransactionBase):
-    max_fee_per_gas: uint
-    max_priority_fee_per_gas: uint
-    access_list: AccessList
-
-
-class Transaction2930Unsigned(TransactionBase):
-    gas_price: uint
-    access_list: AccessList
-
-
-class TransactionLegacyUnsigned(TransactionBase):
-    gas_price: uint
-
-
-# signed transactions
-
-
-class Transaction1559Signed(Transaction1559Unsigned):
-    y_parity: uint
-    r: uint
-    s: uint
-
-
-class Transaction2930Signed(Transaction2930Unsigned):
-    y_parity: uint
-    r: uint
-    s: uint
-
-
-class TransactionLegacySigned(TransactionLegacyUnsigned):
-    v: uint
-    r: uint
-    s: uint
-
-
-# transaction info
-
-
-class Transaction1559Info(Transaction1559Signed):
+    input: HexBytes
     block_hash: hash32
     block_number: uint
-    sender: address
     hash: hash32
     transaction_index: uint
-
-
-class Transaction2930Info(Transaction2930Signed):
-    block_hash: hash32
-    block_number: uint
-    sender: address
-    hash: hash32
-    transaction_index: uint
-
-
-class TransactionLegacyInfo(TransactionLegacySigned):
-    block_hash: hash32
-    block_number: uint
-    sender: address
-    hash: hash32
-    transaction_index: uint
-
-
-TransactionUnsigned = Transaction1559Unsigned | Transaction2930Unsigned | TransactionLegacyUnsigned
-TransactionSigned = Transaction1559Signed | Transaction2930Signed | TransactionLegacySigned
-TransactionInfo = Transaction1559Info | Transaction2930Info | TransactionLegacyInfo
+    r: uint
+    s: uint
+    max_fee_per_gas: Optional[uint] = None
+    max_priority_fee_per_gas: Optional[uint] = None
+    access_list: Optional[AccessList] = None
+    gas_price: Optional[uint] = None
+    y_parity: Optional[uint] = None
+    v: Optional[uint] = None
+    chain_id: Optional[uint] = None
