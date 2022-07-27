fb8875aa4decd0fe84ad0a96187e73826e500d4d
banteg <4562643+banteg@users.noreply.github.com>
2022-07-27 09:41:03 +0400

    feat: add basic types

---
+++ eth_api_spec/types.py
@@ -0,0 +1,29 @@
+from hexbytes import HexBytes
+
+
+class address(str):
+    pass
+
+
+class uint(int):
+    pass
+
+
+class uint256(int):
+    pass
+
+
+class hash32(HexBytes):
+    pass
+
+
+class bytes256(HexBytes):
+    pass
+
+
+class bytes8(HexBytes):
+    pass
+
+
+class byte(int):
+    pass
