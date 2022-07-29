39dd471ccab3f28a7a595b83b35914f6e1f8548a
banteg <4562643+banteg@users.noreply.github.com>
2022-07-29 21:49:46 +0400

    feat: to checksum address

---
+++ snek3/utils.py
@@ -0,0 +1,8 @@
+from eth_hash.backends.pysha3 import keccak256
+from snek3.types.base import address
+
+
+def to_checksum_address(addr) -> address:
+    hash_hex = keccak256(addr[2:].encode()).hex()
+    mask = [int(x, 16) > 7 for x in hash_hex[:40]]
+    return "0x" + "".join([x.upper() if m else x for x, m in zip(addr[2:], mask)])
