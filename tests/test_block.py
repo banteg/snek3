213184b037dd698cca82d4f0b28243e1ceca8b35
banteg <4562643+banteg@users.noreply.github.com>
2023-06-27 04:21:43 +0400

    test: update live tests

--- tests/test_block.py
+++ tests/test_block.py
@@ -1,14 +1,15 @@
 import pytest
 from hexbytes import HexBytes
 
-from snek3.methods.block import GetBlock
+from snek3 import Snek3
 
+snek = Snek3()
 block_number = 15_000_000
-block_hash = "0x" + "".zfill(64)
+block_hash = "0x9a71a95be3fe957457b11817587e5af4c7e24836d5b383c430ff25b9286a457f"
 block_cases = {
     block_number: ("eth_getBlockByNumber", [hex(block_number), False]),
     hex(block_number): ("eth_getBlockByNumber", [hex(block_number), False]),
-    "pending": ("eth_getBlockByHash", ["pending", False]),
+    "latest": ("eth_getBlockByHash", ["latest", False]),
     block_hash: ("eth_getBlockByHash", [block_hash, False]),
     bytes.fromhex(block_hash[2:]): ("eth_getBlockByHash", [block_hash, False]),
     HexBytes(block_hash): ("eth_getBlockByHash", [block_hash, False]),
@@ -17,11 +18,12 @@ block_cases = {
 
 @pytest.mark.parametrize("identifier", block_cases)
 def test_encode_get_block_payload(identifier):
-    handler = GetBlock(identifier)
-    assert handler.method, handler.params == block_cases[identifier]
+    block = snek.get_block(identifier)
+    if identifier != "latest":
+        assert block.number == block_number
 
 
 @pytest.mark.parametrize("identifier", ["bunny", -1])
 def test_encode_get_block_payload_fail(identifier):
     with pytest.raises(ValueError):
-        GetBlock(identifier)
+        snek.get_block(identifier)
